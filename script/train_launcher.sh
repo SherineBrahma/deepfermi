1a7967d67b905f1250f52f5313ad63e9615ede2f
Sherine Brahma <shrnbrahma@yahoo.co.in>
2025-02-23 14:45:54 +0100

    training pipeline script

--- script/train_launcher.sh
+++ script/train_launcher.sh
@@ -18,5 +18,5 @@ cd $code_directory
 CUDA_VISIBLE_DEVICES=$cuda_device python main.py --project_name=$pretraining_folder  --dataset_file_name=$dataset_file_name --build_dataset_flag True --mode='pre_training' --train_from_ckpt False  --cross_val_flag True --cross_val_k=$cross_val_k --cross_val_fold=$cross_val_fold --unet_lr=10e-4 --unet_wd=10e-8
 # Model-based training
 mkdir $experiments_directory/$model_based_training_folder
-cp $experiments_directory/$pretraining_folder/model/unet/unet_iter_4000 $experiments_directory/$model_based_training_folder/unet_load
+cp $experiments_directory/$pretraining_folder/unet $experiments_directory/$model_based_training_folder/unet_load
 CUDA_VISIBLE_DEVICES=$cuda_device python main.py --project_name=$model_based_training_folder  --dataset_file_name=$dataset_file_name --build_dataset_flag False --mode='fine_tuning' --train_from_ckpt True  --cross_val_flag True --cross_val_k=$cross_val_k --cross_val_fold=$cross_val_fold --unet_lr=10e-5 --unet_wd=10e-12
